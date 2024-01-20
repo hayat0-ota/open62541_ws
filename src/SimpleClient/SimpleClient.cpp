637e7901c99c0106f3800996c869cc29ffe46bd2
HAYATO OTA <hayato-ota@mamezou.com>
2024-01-20 17:11:52 +0900

    サーバに登録した関数を呼び出す処理を追加

--- src/SimpleClient/SimpleClient.cpp
+++ src/SimpleClient/SimpleClient.cpp
@@ -46,7 +46,35 @@ void writeSampleVariable(UA_Client* client) {
 /// サーバに登録している関数を実行する
 /// </summary>
 void invokeMethod(UA_Client* client) {
+    UA_Variant input;
+    UA_Int32 argValue = 32; // 追加する値（delta）
 
+    UA_Variant_init(&input);
+    UA_Variant_setScalarCopy(&input, &argValue, &UA_TYPES[UA_TYPES_INT32]);
+
+    // 戻り値用の変数
+    size_t outputSize;
+    UA_Variant* output;
+    
+    // 関数を呼び出す
+    UA_StatusCode retval = UA_Client_call(
+        client, 
+        UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+        UA_NODEID_STRING(1, (char*)"addIncreaseVarNodeId"),
+        1,              // 入力個数 
+        &input,         // 入力データ配列
+        &outputSize,    // 出力データ個数
+        &output         // 出力データ配列
+    );
+
+    if (retval == UA_STATUSCODE_GOOD) {
+        printf("Method call was successful, and %lu returned values available.\n", (unsigned long)outputSize);
+        UA_Array_delete(output, outputSize, &UA_TYPES[UA_TYPES_VARIANT]);
+    }
+    else {
+        printf("Method call was unsuccessful, and %x returned values available.\n", retval);
+    }
+    UA_Variant_clear(&input);
 }
 
 
@@ -66,14 +94,15 @@ int main()
 
     // 変数の値を読みだす
     readSampleVariable(client);
+    printf("Press any key to continue ...");
+    getchar();
 
     // 変数の値を書き込む
-    writeSampleVariable(client);
+    // writeSampleVariable(client);
 
     // 関数にアクセスする
-    // invokeMethod(client);
-
-    // 
+    invokeMethod(client);
+    printf("Press any key to continue ...");
     getchar();
 
     return EXIT_SUCCESS;

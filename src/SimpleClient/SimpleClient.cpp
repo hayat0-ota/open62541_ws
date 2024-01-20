9b151ff26fc0396c6c4577415d17f37ddd77080e
HAYATO OTA <hayato-ota@mamezou.com>
2024-01-20 17:37:20 +0900

    変数の値を書き換える処理を追加

--- src/SimpleClient/SimpleClient.cpp
+++ src/SimpleClient/SimpleClient.cpp
@@ -20,14 +20,15 @@ void readSampleVariable(UA_Client* client) {
     UA_Int32 value = 0;
     UA_Variant* var = UA_Variant_new();
     UA_StatusCode retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(1, (char*)"SampleVarNodeId"), var);    // NodeIdを指定する
+    
     if (retval == UA_STATUSCODE_GOOD && UA_Variant_isScalar(var) &&
         var->type == &UA_TYPES[UA_TYPES_INT32])
     {
         value = *(UA_Int32*)var->data;
-        printf("the value of SampleVariable: %d", value);
+        printf("the value of SampleVariable: %d\n", value);
     }
     else {
-        printf("Failed to read SampleVariable");
+        printf("Failed to read SampleVariable\n");
     }
 }
 
@@ -36,8 +37,16 @@ void readSampleVariable(UA_Client* client) {
 /// 変数の値を書き込む
 /// </summary>
 /// <param name="client">クライアントインスタンスアドレス</param>
-void writeSampleVariable(UA_Client* client) {
+/// <param name="data">書き換える値</param>
+UA_StatusCode writeSampleVariable(UA_Client* client, UA_Int32 newValue) {
+    UA_Variant newValueVariant;
+    UA_Variant_setScalar(&newValueVariant, &newValue, &UA_TYPES[UA_TYPES_INT32]);
+    UA_StatusCode retval = UA_Client_writeValueAttribute(client, UA_NODEID_STRING(1, (char*)"SampleVarNodeId"), &newValueVariant);
 
+    if (retval != UA_STATUSCODE_GOOD) {
+        printf("Failed to write sample variable value, returned %x\n", retval);
+    }
+    return retval;
 }
 
 
@@ -59,8 +68,8 @@ void invokeMethod(UA_Client* client) {
     // 関数を呼び出す
     UA_StatusCode retval = UA_Client_call(
         client, 
-        UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
-        UA_NODEID_STRING(1, (char*)"addIncreaseVarNodeId"),
+        UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),   // オブジェクトID
+        UA_NODEID_STRING(1, (char*)"addIncreaseVarNodeId"), // メソッドのID
         1,              // 入力個数 
         &input,         // 入力データ配列
         &outputSize,    // 出力データ個数
@@ -86,24 +95,30 @@ int main()
     UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
     if (retval != UA_STATUSCODE_GOOD) {
         UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
-            "The connection failed with status code %s",
+            "The connection failed with status code %s\n",
             UA_StatusCode_name(retval));
         UA_Client_delete(client);
         return 0;
     }
 
     // 変数の値を読みだす
-    readSampleVariable(client);
-    printf("Press any key to continue ...");
-    getchar();
+    printf("readSampleVariable()\n");
+    readSampleVariable(client); // 値を取得する
 
     // 変数の値を書き込む
-    // writeSampleVariable(client);
+    printf("=== writeSampleVariable() ===\n");
+    writeSampleVariable(client, -1);
+    readSampleVariable(client); // 結果を出力する
 
     // 関数にアクセスする
+    printf("=== invokeMethod() ===\n");
     invokeMethod(client);
+    readSampleVariable(client); // 結果を出力する
+
+
     printf("Press any key to continue ...");
-    getchar();
+    char* str = (char*)"";
+    std::cin >> str;
 
     return EXIT_SUCCESS;
 }
